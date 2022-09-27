fd688fd8589f24fd3d6ef45a9a721b12fc4f9af2
Hamid Escalera <hamid.uxw@gmail.com>
2022-09-26 21:23:46 -0500

    updates

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,8 @@
 const Button = (props) => {
 	return (
 		<button
-			className='bg-indigo-600 text-white font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 
-      duration-500'
+			className='bg-sky-600 text-white py-2 px-6 rounded md:ml-8 hover:bg-sky-400 
+      duration-500 uppercase'
 		>
 			{props.children}
 		</button>
